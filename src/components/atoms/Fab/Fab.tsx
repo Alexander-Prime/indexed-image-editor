3a3035a4afe8d5313701b3808eec0ddfbf4ec908
Alexander Prime <rainesabaal@gmail.com>
2017-11-20 06:18:53 -0700

    Add Fab

---
+++ src/components/atoms/Fab/Fab.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./Fab.scss";
+
+interface Props {}
+
+const Fab = (_: Props) => {
+  return <button className="fab">#</button>;
+};
+
+export { Fab };
--- src/components/organisms/CanvasView/CanvasView.tsx
+++ src/components/organisms/CanvasView/CanvasView.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 
+import { Fab } from "components/atoms";
+
 import "./CanvasView.scss";
 
 interface Props {}
 
 const CanvasView = (_: Props) => {
-  return <div className="canvasView" />;
+  return (
+    <div className="canvasView">
+      <Fab />
+    </div>
+  );
 };
 
 export { CanvasView };
