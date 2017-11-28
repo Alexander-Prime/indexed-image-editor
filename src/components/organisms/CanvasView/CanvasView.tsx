3276385dd4dc26980afec33636a00cf8749a1fd1
Alexander Prime <rainesabaal@gmail.com>
2017-11-28 05:18:23 -0700

    Add canvas grid overlay & temp background color

--- src/components/organisms/CanvasView/CanvasView.tsx
+++ src/components/organisms/CanvasView/CanvasView.tsx
@@ -6,10 +6,19 @@ import "./CanvasView.scss";
 
 interface Props {}
 
-const CanvasView = (_: Props) => (
-  <div className="canvasView">
-    <Fab className="canvasView-gridToggle" />
-  </div>
-);
+const CanvasView = (_: Props) => {
+  const zoomFactor = 32;
+  return (
+    <div className="canvasView">
+      <Fab className="canvasView-gridToggle" />
+      <div className="canvasView-gridOverlay" style={gridStyle(zoomFactor)} />
+    </div>
+  );
+};
+
+const gridStyle = (zoomFactor: number) => ({
+  opacity: (zoomFactor - 4) / 64,
+  backgroundSize: `${zoomFactor}px ${zoomFactor}px`,
+});
 
 export { CanvasView };
