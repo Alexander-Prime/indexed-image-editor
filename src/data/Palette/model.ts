fbe4929770cc9784a9fb52eaa97d3188884b43cb
Alexander Prime <rainesabaal@gmail.com>
2017-12-04 13:13:53 -0700

    Fix canvas layout

--- src/data/Palette/model.ts
+++ src/data/Palette/model.ts
@@ -8,7 +8,7 @@ interface PaletteProps {
 }
 
 class Palette extends Record<PaletteProps>({
-  colors: List.of([255, 255, 255] as [number, number, number]),
+  colors: List.of([0, 0, 0] as [number, number, number]),
   ramps: Map.of(0, new Ramp({ name: "Ramp 1" })),
 }) {}
 
