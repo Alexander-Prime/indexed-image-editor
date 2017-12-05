573cf98f3130aa8d23599003c0e940782baef13a
Alexander Prime <rainesabaal@gmail.com>
2017-12-05 09:52:48 -0700

    Add basic drawing controls

--- src/components/organisms/CanvasView/CanvasView.tsx
+++ src/components/organisms/CanvasView/CanvasView.tsx
@@ -1,3 +1,4 @@
+import { List, Repeat } from "immutable";
 import React from "react";
 import { connect } from "react-redux";
 
@@ -13,16 +14,35 @@ interface StateProps {
   image: Image;
   frame: Frame;
   zoom: number;
+  paletteIndex: number;
 }
 
 interface OwnProps {}
 
 type Props = StateProps & OwnProps;
 
-class CanvasViewInternal extends React.PureComponent<Props> {
+interface State {
+  drawMask: List<boolean>;
+  drawMode: "draw" | "erase";
+}
+
+class CanvasViewInternal extends React.PureComponent<Props, State> {
   private ctx: CanvasRenderingContext2D;
 
+  constructor(props: Props) {
+    super(props);
+    const { image } = props;
+    this.state = {
+      drawMask: this.createMask(image),
+      drawMode: "draw",
+    };
+  }
+
   componentDidMount() {
+    this.renderPixels();
+  }
+
+  componentDidUpdate() {
     this.renderPixels();
   }
 
@@ -38,11 +58,18 @@ class CanvasViewInternal extends React.PureComponent<Props> {
         <div className="canvasView-frame">
           <canvas
             className="canvasView-frame-canvas"
-            onClick={console.log}
             width={image.width}
             height={image.height}
             style={{ width: elementWidth, height: elementHeight }}
-            ref={canvas => (this.ctx = canvas!.getContext("2d")!)}
+            ref={canvas => {
+              if (canvas) {
+                this.ctx = canvas!.getContext("2d")!;
+              }
+            }}
+            onContextMenu={this.onContextMenu}
+            onMouseDown={this.onDraw}
+            onMouseMove={this.onDraw}
+            onMouseUp={this.onFinishDraw}
           />
           <div
             className="canvasView-frame-gridOverlay"
@@ -54,21 +81,78 @@ class CanvasViewInternal extends React.PureComponent<Props> {
   }
 
   private renderPixels() {
-    const { image, frame } = this.props;
-    const pixels = frame.pixels.reduce((prior: number[], i?: number) => {
-      if (i === undefined) {
-        prior.push(0, 0, 0, 0);
-        return prior;
-      }
-      prior.push(...image.palette.colors.get(i, [0, 0, 0]), 255);
-      return prior;
-    }, []);
-    const bytes = new Uint8ClampedArray(pixels);
+    const { image, frame, paletteIndex } = this.props;
+    const bytes = new Uint8ClampedArray(
+      frame.pixels.reduce(
+        (prior: number[], c: number | undefined, i: number) => {
+          const { drawMask, drawMode } = this.state;
+          if (drawMask.get(i)) {
+            if (drawMode === "draw") {
+              prior.push(
+                ...image.palette.colors.get(paletteIndex, [0, 0, 0]),
+                255,
+              );
+            } else {
+              prior.push(0, 0, 0, 0);
+            }
+          } else {
+            if (c === undefined) {
+              prior.push(0, 0, 0, 0);
+            } else {
+              prior.push(...image.palette.colors.get(c, [0, 0, 0]), 255);
+            }
+          }
+          return prior;
+        },
+        [],
+      ),
+    );
     this.ctx.putImageData(
       new ImageData(bytes, image.width, image.height),
       0,
       0,
     );
+  }
+
+  private onContextMenu = (ev: React.MouseEvent<HTMLCanvasElement>) => {
+    ev.preventDefault();
+    return false;
+  };
+
+  private onDraw = (ev: React.MouseEvent<HTMLCanvasElement>) => {
+    if (ev.buttons === 1 || ev.buttons === 2) {
+      this.maskPixels([
+        Math.floor(ev.nativeEvent.offsetX / 32),
+        Math.floor(ev.nativeEvent.offsetY / 32),
+      ]);
+    }
+    if (ev.buttons === 1 /* LMB */) {
+      this.setState({ drawMode: "draw" });
+    }
+    if (ev.buttons === 2 /* RMB */) {
+      this.setState({ drawMode: "erase" });
+    }
+  };
+
+  private onFinishDraw = (_: React.MouseEvent<HTMLCanvasElement>) => {
+    this.setState({
+      drawMask: this.createMask(this.props.image),
+    });
+  };
+
+  private createMask(image: Image) {
+    return Repeat(false, image.width * image.height).toList();
+  }
+
+  private maskPixels(...coords: Array<[number, number]>) {
+    const { image } = this.props;
+    this.setState({
+      drawMask: this.state.drawMask.withMutations(mask => {
+        coords.forEach(coord =>
+          mask.set(coord[0] + coord[1] * image.width, true),
+        );
+      }),
+    });
   }
 }
 
@@ -81,6 +165,7 @@ const mapStateToProps = (state: AppState): StateProps => ({
   image: state.image,
   frame: state.image.strip.frames.first()!,
   zoom: state.zoom,
+  paletteIndex: 0,
 });
 
 const CanvasView = connect(mapStateToProps)(CanvasViewInternal);
